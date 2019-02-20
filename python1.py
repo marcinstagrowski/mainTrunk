85ed6ca39a5ebd34d967dbbd090512858d12dc6f
marcinstagrowski <47832797+marcinstagrowski@users.noreply.github.com>
2019-02-20 22:54:08 +0100

    double brackets

--- python1.py
+++ python1.py
@@ -1,10 +1,10 @@
 import datetime
 now = datetime.datetime.now()
-print "-" * 25
+print ("-" * 25)
 print now
 print now.year
 print now.month
 print now.day
 print now.hour
 print now.minute
-print now.second
+print ((now.second))
